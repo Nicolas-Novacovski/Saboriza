bcaf3c35af62f7660fc8fbb0daba2680626f2b94
Nicolas-Novacovski <nicolas.vendrami@gmail.com>
2025-07-10 10:06:33 -0300

    add: projeto

---
+++ Saboriza/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Saboriza.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
