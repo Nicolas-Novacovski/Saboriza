e78d64af81cf61861c7b76e7fe6eed4d47f628a5
dominique.valter <dominique.mariah.vltr@gmail.com>
2025-07-10 10:33:47 -0300

    Add: models de usuario e base model

---
+++ Saboriza/Models/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Saboriza.Models
+{
+    public class BaseModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public int? DeletedBy { get; set; }
+    }
+}
---
+++ Saboriza/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Saboriza.Models
+{
+
+    [table("Usuarios")]
+    public class Usuario : BaseModel { 
+
+        public int Id { get; set; }
+        public string NomeUsuario { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Senha { get; set; }
+        public int Role { get; set; }
+
+
+    }
+
+}
